b4562bd04960191bd876d1f97779a32e8cd5f2dc
Oladele-israel <omoleoladele43@gmail.com>
2025-01-13 13:09:15 +0100

    first commit

---
+++ backend/src/Routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  validateToken,
+} from "../controllers/user.controller.js";
+import { checkAndRenewToken } from "../middleware/validateToken.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/signup", signup);
+userRouter.post("/login", login);
+userRouter.post("/logout", logout);
+userRouter.get("/validateUser", checkAndRenewToken, validateToken);
+
+export default userRouter;
